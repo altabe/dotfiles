5e2dd2127e0ca0e92a60ec6eaedcd060a0adbb0b
Tomer Ben Altabe <tomerbe@mobileye.com>
2024-06-21 23:05:28 +0300

    disable command completion plugin that I am not sure I need

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -4,4 +4,5 @@ return {
   { "dstein64/vim-startuptime", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "smolck/command-completion.nvim", enabled = false },
 }
