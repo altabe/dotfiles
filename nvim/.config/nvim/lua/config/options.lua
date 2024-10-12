7d5a50f970bcdb00f041e88edcb7901f643a0c1f
Tomer Ben Altabe <tomerbe@mobileye.com>
2024-10-12 21:04:07 +0300

    starting to config with Josean Martinez video

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,27 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+
+opt.autoindent = true
+
+opt.wrap = true
+
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- disable ignore case when using capital letters
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
+
+
