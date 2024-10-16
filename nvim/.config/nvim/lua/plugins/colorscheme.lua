37ce4ecc60ed89862eaf299685319f423602d8a1
Tomer Ben Altabe <tomerbe@mobileye.com>
2024-10-16 09:29:19 +0300

    comments + config style of kickstart

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
-return {
+return { -- You can easily change to a different colorscheme.
   "catppuccin/nvim",
   name = "catppuccin",
-  priority = 1000,
+  priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
-    vim.cmd("colorscheme catppuccin-mocha")
-  end
+    vim.cmd.colorscheme("catppuccin-mocha")
+    vim.cmd.hi("Comment gui=none")
+  end,
 }
