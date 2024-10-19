80bcdf67c9ef6130bdff2bba467dc5733a166165
Tomer Ben Altabe <tomerbe@mobileye.com>
2024-10-19 15:32:54 +0300

    completion for cmdline

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -115,5 +115,17 @@ return { -- Autocompletion
 			}
 		})
 
+		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline(':', {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = 'path' }
+			}, {
+				{ name = 'cmdline' }
+			}),
+			completion = {
+				completeopt = 'menu,menuone,noselect'
+			}
+		})
 	end,
 }
