d91ec74a361a8d12d45fc61cf980d9c2beaf6016
Tomer Ben Altabe <tomerbe@mobileye.com>
2024-10-19 15:31:49 +0300

    insert completion with tab

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -69,6 +69,7 @@ return { -- Autocompletion
 				--  This will expand snippets if the LSP sent a snippet.
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				['<CR>'] = cmp.mapping.confirm { select = true },
+				['<Tab>'] = cmp.mapping.confirm({ behaviour = cmp.ConfirmBehavior.Insert, select = true }),
 
 				["<A-Space>"] = cmp.mapping.complete({}),
 
