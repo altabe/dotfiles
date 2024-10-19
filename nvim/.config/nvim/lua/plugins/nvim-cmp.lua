1691a84432eecc0acaabf15e67732634ae7eafc6
Tomer Ben Altabe <tomerbe@mobileye.com>
2024-10-19 15:30:10 +0300

    add dependecies of buffer and cmdline completion

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -32,7 +32,9 @@ return { -- Autocompletion
 		--  nvim-cmp does not ship with all sources by default. They are split
 		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
 	},
 	config = function()
 		-- See `:help cmp`
